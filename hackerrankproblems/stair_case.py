01e92af19b35a6397bfc3e69df7bd95fab1e1047
RowlandOti <rowlandotienoo@gmail.com>
2017-11-01 00:34:35 +0300

    feat: add solution for printed staircase problem
    Signed-off-by: RowlandOti <rowlandotienoo@gmail.com>

---
+++ hackerrankproblems/stair_case.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+# Consider a staircase of size N =4.
+
+# Observe that its base and height are both equal to N, and the image is drawn using `#`
+# symbols and spaces. The last line is not preceded by any spaces. Write a program that prints a staircase of size N.
+
+print("**What is the size of the Staircase**")
+N = int(input().strip())
+
+# Method 1
+for i in range(1, N + 1):
+    print(' ' * (N - i) + '#' * i)
+
+# Method 2
+[print(' ' * (N - i) + '#' * i) for i in range(1, N + 1)]
