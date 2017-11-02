701a6f2eb0613db8f5b8990519759ca4194ffbc7
RowlandOti <rowlandotienoo@gmail.com>
2017-11-02 12:21:17 +0300

    feat: find max element and find its count
    Signed-off-by: RowlandOti <rowlandotienoo@gmail.com>

--- hackerrankproblems/birthday_cake.py
+++ hackerrankproblems/birthday_cake.py
@@ -3,15 +3,21 @@
 # Given the height I for each individual candle, find and print the number of candles she can successfully blow out.
 from collections import Counter
 
+# Method 1
+def birthdayCakeBlownCandles1(n, height_arr):
+    return height_arr.count(max(height_arr))
 
 # Method 2
-def birthdayCakeBlownCandles(n, height_arr):
-    return height_arr.count(max(height_arr))
+def birthdayCakeBlownCandles2(n, height_arr):
+    return max(Counter(height_arr).items())[1]
 
 print("**What is Colleen's Age**")
 n = int(input().strip())
 print("**Enter the height of the candles**")
 height_arr = [int(x) for x in input().strip().split(' ')]
 
-result = birthdayCakeBlownCandles(n, height_arr)
+result = birthdayCakeBlownCandles1(n, height_arr)
+print(result)
+
+result = birthdayCakeBlownCandles2(n, height_arr)
 print(result)
