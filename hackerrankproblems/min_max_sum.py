3ebcf39ad846b6c8bc97ec3860aacd038309797b
Umang Singh <43104796+InsideU@users.noreply.github.com>
2018-10-02 12:14:06 +0530

    simple
    An easy and simple way to find the answer i guess :))

--- hackerrankproblems/min_max_sum.py
+++ hackerrankproblems/min_max_sum.py
@@ -2,17 +2,15 @@
 # calculated by summing exactly four of the five integers.
 # Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 #simple way without any load to the interpretor and without importing library can be 
+#!/bin/python3
 l=[]
-m=[]
-count=0
 u=0
 n=5
 l=list(map(int,input().split()))
-    
 for i in range(0,n):
     u+=l[i]
 z=max(l)
 x=min(l)
-print(u-z)
-print(u-x)
+print(u-z,u-x)
+
 
